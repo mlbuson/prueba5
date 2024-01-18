842f7984c43365880bccae9b32a8551d51a43808
mlbuson <62416087+mlbuson@users.noreply.github.com>
2024-01-18 13:33:00 -0300

    Update Controlador.java

--- prueba5/src/main/java/com/example/prueba5/Controlador.java
+++ prueba5/src/main/java/com/example/prueba5/Controlador.java
@@ -1,6 +1,4 @@
 package com.example.prueba5;
-
-// StringManipulationController.java
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controlador {
 
     @PostMapping("/manipulateStringArray")
-    public String manipulateStringArray(@RequestBody String[] inputArray) {
-        // Lógica para unir elementos, convertir a minúsculas y enviar a stdout
+    public String manipulateStringArray(@RequestBody String[] inputArray) {        
         String result = manipulateStrings(inputArray);
-        System.out.println(result); // Imprimir en stdout
+        System.out.println(result);
         return result;
     }
     /*@GetMapping("/") -- Esto fue para probar que la config este bien.
@@ -24,11 +21,9 @@ public class Controlador {
     private String manipulateStrings(String[] inputArray) {
         StringBuilder resultBuilder = new StringBuilder();
 
-        for (String str : inputArray) {
-            // Unir con espacios y convertir a minúsculas
+        for (String str : inputArray) {            
             resultBuilder.append(str.toLowerCase()).append(" ");
         }
-
-        return resultBuilder.toString().trim(); // Eliminar el espacio al final
+        return resultBuilder.toString().trim();
     }
 }
